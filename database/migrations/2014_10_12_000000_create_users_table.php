be7159ccdb2fc9d617dd11a3ce5b227fa178c928
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-08-31 11:30:26 +0800

    added authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->boolean('is_tabulator');
+            $table->boolean('is_judge');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
