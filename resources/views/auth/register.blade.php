131a848701f8b26f37f7a07a0a1520ee0e074c6b
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-08-31 12:06:16 +0800

    registration and login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,10 +40,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('email') }}" required>
+                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required>
 
                                 @if ($errors->has('username'))
                                     <span class="invalid-feedback" role="alert">
