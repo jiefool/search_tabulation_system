d044893c4f217576369bba1c10c95f53cf273335
Jay Paul Aying <jaypaulaying5@gmail.com>
2018-08-31 11:54:43 +0800

    username and categories

--- database/migrations/2018_08_31_033434_create_categories_table.php
+++ database/migrations/2018_08_31_033434_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->float('weight', 1, 2);
+            $table->float('weight', 8, 2);
             $table->timestamps();
         });
     }
